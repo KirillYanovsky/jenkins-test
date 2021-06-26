932a8ccc9f1864ea54b746271938d06ef8ecda7a
root <root@CO-1214.codeoasis.com>
2021-06-26 11:19:35 +0300

    init commit

---
+++ __test__/index.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("../index");
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
+
+afterAll(async() => {
+	await app.close();
+});
